9e67264ddde60941935c94b07af05b9f6eb51d48
wellmoredrone <85592388+wellmoredrone@users.noreply.github.com>
2022-06-17 08:16:02 -0400

    init

---
+++ data.js
@@ -0,0 +1,123 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-area-1",
+      "name": "Area 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5500,
+      "initialViewParameters": {
+        "yaw": -0.05542897348154696,
+        "pitch": 0.3307606275771686,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6497780401917339,
+          "pitch": -0.07990038846003955,
+          "rotation": 0,
+          "target": "1-area-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.1372255450951947,
+          "pitch": 0.49235240862540763,
+          "title": "Eagle A",
+          "text": "On Eagle A now drilling down to Eagle B."
+        },
+        {
+          "yaw": -1.8121773653497577,
+          "pitch": 0.18637155505052405,
+          "title": "Development Area",
+          "text": "Drilling for development area for future pits."
+        }
+      ]
+    },
+    {
+      "id": "1-area-2",
+      "name": "Area 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5500,
+      "initialViewParameters": {
+        "yaw": -0.18392956427216944,
+        "pitch": 0.3708964297256596,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.0109531371485243,
+          "pitch": 0.06622328059306604,
+          "rotation": 0,
+          "target": "0-area-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.3481830410304845,
+          "pitch": 0.3129160913329052,
+          "title": "Pond Creek",
+          "text": "Pond Creek pit for June 2022"
+        }
+      ]
+    }
+  ],
+  "name": "Long Branch 6-14-22",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
